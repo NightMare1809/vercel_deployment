92eb48a1a40bcf7f852540933d58e394db12e280
Vikram Sutar <130957830+NightMare1809@users.noreply.github.com>
2025-02-11 00:30:09 +0530

    updated

--- vercel_deployment/client/src/App.js
+++ vercel_deployment/client/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div class = "Container">
-      <h1>Bacha Sorry mai kam krra tha</h1>
+      <h1>React plus Node js Porject Startup</h1>
     </div>
   );
 }
